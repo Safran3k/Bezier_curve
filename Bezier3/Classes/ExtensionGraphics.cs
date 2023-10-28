1781d40c604902e1e96aa40e509ce6dabb94615a
safran3k <futo.ricsi@gmail.com>
2023-10-28 14:51:42 +0200

    Update bezier curve

---
+++ Bezier3/Classes/ExtensionGraphics.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrafikaAlap
+{
+    public static class ExtensionGraphics
+    {
+        public delegate double RtoRP(double t, double p);
+        public static void DrawParametricCurveP(this Graphics g, Pen pen, RtoRP x, RtoRP y, double a, double b, double p, int n = 500)
+        {
+            double t = a;
+            double h = (b - a) / n;
+            Vector2 v0 = new Vector2(x(t, p), y(t, p));
+            Vector2 v1;
+
+            while (t < b)
+            {
+                t += h;
+                v1 = new Vector2(x(t, p), y(t, p));
+                g.DrawLine(pen, (float)v0.x, (float)v0.y, (float)v1.x, (float)v1.y);
+                v0 = v1;
+            }
+        }
+
+        public static void DrawBezier3P(this Graphics g, Pen pen, Vector2 p0, Vector2 p1, Vector2 p2, Vector2 p3, double p)
+        {
+            g.DrawParametricCurveP(pen,
+                (t, pValue) => 
+                    Bezier3P.B0(t, pValue) * p0.x + 
+                    Bezier3P.B1(t, pValue) * p1.x + 
+                    Bezier3P.B2(t, pValue) * p2.x + 
+                    Bezier3P.B3(t, pValue) * p3.x,
+                (t, pValue) => 
+                    Bezier3P.B0(t, pValue) * p0.y + 
+                    Bezier3P.B1(t, pValue) * p1.y + 
+                    Bezier3P.B2(t, pValue) * p2.y + 
+                    Bezier3P.B3(t, pValue) * p3.y,
+                0.0, 1.0, p);
+        }
+
+    }
+}
---
+++ Bezier3/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace GrafikaAlap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.canvas = new System.Windows.Forms.PictureBox();
+            this.lblLambda = new System.Windows.Forms.Label();
+            this.sbLambda = new System.Windows.Forms.HScrollBar();
+            this.lbLambdaValue = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // canvas
+            // 
+            this.canvas.BackColor = System.Drawing.Color.White;
+            this.canvas.Location = new System.Drawing.Point(12, 12);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(781, 426);
+            this.canvas.TabIndex = 0;
+            this.canvas.TabStop = false;
+            this.canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.canvas_Paint);
+            this.canvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseDown);
+            this.canvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseMove);
+            this.canvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseUp);
+            // 
+            // lblLambda
+            // 
+            this.lblLambda.AutoSize = true;
+            this.lblLambda.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblLambda.Location = new System.Drawing.Point(817, 12);
+            this.lblLambda.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblLambda.Name = "lblLambda";
+            this.lblLambda.Size = new System.Drawing.Size(92, 20);
+            this.lblLambda.TabIndex = 2;
+            this.lblLambda.Text = "lambda = ";
+            // 
+            // sbLambda
+            // 
+            this.sbLambda.LargeChange = 1;
+            this.sbLambda.Location = new System.Drawing.Point(801, 40);
+            this.sbLambda.Maximum = 500;
+            this.sbLambda.Name = "sbLambda";
+            this.sbLambda.Size = new System.Drawing.Size(238, 36);
+            this.sbLambda.TabIndex = 3;
+            this.sbLambda.Value = 250;
+            this.sbLambda.ValueChanged += new System.EventHandler(this.sbLambda_ValueChanged);
+            // 
+            // lbLambdaValue
+            // 
+            this.lbLambdaValue.AutoSize = true;
+            this.lbLambdaValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lbLambdaValue.Location = new System.Drawing.Point(902, 12);
+            this.lbLambdaValue.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbLambdaValue.Name = "lbLambdaValue";
+            this.lbLambdaValue.Size = new System.Drawing.Size(0, 20);
+            this.lbLambdaValue.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(872, 127);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "[0;0]";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(817, 127);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "v0 = ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(872, 161);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "[0;0]";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(817, 161);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "v1 = ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(872, 193);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "[0;0]";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(817, 193);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 20);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "v2 = ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.Location = new System.Drawing.Point(872, 226);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(47, 20);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "[0;0]";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label8.Location = new System.Drawing.Point(817, 226);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(51, 20);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "v3 = ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1046, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbLambdaValue);
+            this.Controls.Add(this.sbLambda);
+            this.Controls.Add(this.lblLambda);
+            this.Controls.Add(this.canvas);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox canvas;
+        private System.Windows.Forms.Label lblLambda;
+        private System.Windows.Forms.HScrollBar sbLambda;
+        private System.Windows.Forms.Label lbLambdaValue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
